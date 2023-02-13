0015155d01a41dcf04a2c18fca3e20e5a4f29201
poojajayan2 <115702958+poojajayan2@users.noreply.github.com>
2023-02-13 15:14:15 +0530

    Delete colors 1.py

--- colors 1.py
+++
@@ -1,9 +0,0 @@
-
-
-color_list_1  = ['blue','red','white','violet']    
-color_list_2 = ['white','blue','yellow','green']    
-a = set(color_list_1 + color_list_2)          
-for n in a:        
-if  n in color_list_2:            
-c = set(a - set(color_list_2))              
-return c
